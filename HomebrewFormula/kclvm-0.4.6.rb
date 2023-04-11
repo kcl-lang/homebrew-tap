77195c2cb425ad262163846a62169022844f2020
Peefy <xpf6677@163.com>
2023-04-11 15:41:57 +0800

    fix: wrong links for kclvm-v0.4.6-beta.1

--- HomebrewFormula/kclvm-0.4.6.rb
+++ HomebrewFormula/kclvm-0.4.6.rb
@@ -6,18 +6,18 @@ class Kclvm046 < Formula
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6/kclvm-v0.4.6-beta.1-darwin-arm64.tar.gz"
+        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6-beta.1/kclvm-v0.4.6-beta.1-darwin-arm64.tar.gz"
         sha256 "942f73e1f9492a42778de4c5cbf44a4c94ba6df8a2fed776f3a8f39457e58739"
       end
       if Hardware::CPU.intel?
-        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6/kclvm-v0.4.6-beta.1-darwin-amd64.tar.gz"
+        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6-beta.1/kclvm-v0.4.6-beta.1-darwin-amd64.tar.gz"
         sha256 "bb4a4b860da11cf989ccba7a4c8011660ac2193e1c0f2a382ba11ab398c740b5"
       end
     end
   
     on_linux do
       if Hardware::CPU.intel?
-        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6/kclvm-v0.4.6-beta.1-linux-amd64.tar.gz"
+        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6-beta.1/kclvm-v0.4.6-beta.1-linux-amd64.tar.gz"
         sha256 "fabb34c1a7bf8bd88453a227665fe1736e041218ba8b991361557bd5120d5ed7"
       end
     end
