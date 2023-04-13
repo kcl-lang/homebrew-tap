bdd6037ee5d2a66bc78782ae69f48361475f8ca4
Peefy <xpf6677@163.com>
2023-04-13 17:56:49 +0800

    feat: update kclvm v0.4.6 release

--- HomebrewFormula/kclvm-0.4.6.rb
+++ HomebrewFormula/kclvm-0.4.6.rb
@@ -6,19 +6,19 @@ class Kclvm046 < Formula
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6-beta.1/kclvm-v0.4.6-beta.1-darwin-arm64.tar.gz"
-        sha256 "942f73e1f9492a42778de4c5cbf44a4c94ba6df8a2fed776f3a8f39457e58739"
+        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6/kclvm-v0.4.6-darwin-arm64.tar.gz"
+        sha256 "7c1f14f12bc5a7d547e79e55ddfe9e92bcdfa0194892261d301f680b20d7feb5"
       end
       if Hardware::CPU.intel?
-        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6-beta.1/kclvm-v0.4.6-beta.1-darwin-amd64.tar.gz"
-        sha256 "bb4a4b860da11cf989ccba7a4c8011660ac2193e1c0f2a382ba11ab398c740b5"
+        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6/kclvm-v0.4.6-darwin-amd64.tar.gz"
+        sha256 "a45e668aea04cd8e14896d14667c8117c72f9e3a109c80498c6141a371dfb0b3"
       end
     end
   
     on_linux do
       if Hardware::CPU.intel?
-        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6-beta.1/kclvm-v0.4.6-beta.1-linux-amd64.tar.gz"
-        sha256 "fabb34c1a7bf8bd88453a227665fe1736e041218ba8b991361557bd5120d5ed7"
+        url "https://github.com/KusionStack/KCLVM/releases/download/v0.4.6/kclvm-v0.4.6-linux-amd64.tar.gz"
+        sha256 "f1973be7b8a4d29d98794575707a054c24ff8ac30b23414cd3a32e661abeb919"
       end
     end
   
