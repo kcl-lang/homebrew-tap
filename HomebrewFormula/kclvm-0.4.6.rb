774285ac8c40ae793e8f22a7f77e99bd24282e3e
Peefy <xpf6677@163.com>
2023-04-17 17:42:30 +0800

    chore: update lib install

--- HomebrewFormula/kclvm-0.4.6.rb
+++ HomebrewFormula/kclvm-0.4.6.rb
@@ -25,7 +25,7 @@ class Kclvm046 < Formula
     def install
         prefix.install_metafiles
         libexec.install Dir["*"]
-        lib.install Dir["#{libexec}/lib/libkclvm_*.*"]
+        lib.install "#{libexec}/lib/libkclvm_*.*"
         bin.install Dir["#{libexec}/bin/kcl*"]
         bin.env_script_all_files libexec/"bin", KCL_PLUGINS_ROOT: "#{libexec}/plugins"
     end
