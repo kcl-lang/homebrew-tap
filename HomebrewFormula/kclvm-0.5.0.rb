2eb1fc8de56c1e05da94a15d80b9fb4ab14a737d
peefy <xpf6677@163.com>
2023-07-03 13:41:21 +0800

    chore: bump version to v0.5.0-beta.1

--- HomebrewFormula/kclvm-0.5.0.rb
+++ HomebrewFormula/kclvm-0.5.0.rb
@@ -1,24 +1,24 @@
 class Kclvm050 < Formula
   desc "A constraint-based record & functional language mainly used in configuration and policy scenarios."
   homepage "https://kcl-lang.io"
-  version "0.5.0-alpha.4"
+  version "0.5.0-beta.1"
   license "Apache License"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.0-alpha.4/kclvm-v0.5.0-alpha.4-darwin-arm64.tar.gz"
-      sha256 "7525b94c825b07f851eb29f1a03a39277a31bc0aab123e70711663d43230fecf"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.0-beta.1/kclvm-v0.5.0-beta.1-darwin-arm64.tar.gz"
+      sha256 "1f535d19ff492119d4e4a1cdac53f9f9a36744e1bfc11fed10d5e0d5743d5418"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.0-alpha.4/kclvm-v0.5.0-alpha.4-darwin-amd64.tar.gz"
-      sha256 "9ea3f4ec325bd923d5a481d3e7504ff8be15befe35872252cc10a656b0a585b7"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.0-beta.1/kclvm-v0.5.0-beta.1-darwin-amd64.tar.gz"
+      sha256 "53077d4653cb8c2d886cb8197722954873beef9e880b959d0510b4588f82607b"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.0-alpha.4/kclvm-v0.5.0-alpha.4-linux-amd64.tar.gz"
-      sha256 "4bbb4b46843597b7f0ee18f7b248ed69c50460ceee66788cb0727d7a48cdd025"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.0-beta.1/kclvm-v0.5.0-beta.1-linux-amd64.tar.gz"
+      sha256 "d8939987ec4f34d3cb340154790d839120d3f0403977ac8f937dde8104353ce3"
     end
   end
 
