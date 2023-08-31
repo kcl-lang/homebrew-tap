799a56bd2a6bede51b3798bda6be3dc87bafe74e
peefy <xpf6677@163.com>
2023-08-31 19:37:08 +0800

    chore: bump kcl version to v0.5.6

--- HomebrewFormula/kcl.rb
+++ HomebrewFormula/kcl.rb
@@ -1,24 +1,24 @@
 class Kcl < Formula
   desc "A constraint-based record & functional language mainly used in configuration and policy scenarios."
   homepage "https://kcl-lang.io"
-  version "0.5.5"
+  version "0.5.6"
   license "Apache License"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.5/kclvm-v0.5.5-darwin-arm64.tar.gz"
-      sha256 "408a1043105729c470a19f8c075b1563b271c3a3ca9ade2fb406d4d505edc94f"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.6/kclvm-v0.5.6-darwin-arm64.tar.gz"
+      sha256 "126ba53bf176c524a40f18c9286b919a7419b2f4245758380e45ded78b6e2ff7"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.5/kclvm-v0.5.5-darwin-amd64.tar.gz"
-      sha256 "f7bbc32dd1f1837959957b69d25a9c75777caca9914330c3ba2a919f88e92dd2"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.6/kclvm-v0.5.6-darwin-amd64.tar.gz"
+      sha256 "9a2fca6f789e7ac529a69a33f6613e746c52847ef9a7a4a7354ace2674444593"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.5/kclvm-v0.5.5-linux-amd64.tar.gz"
-      sha256 "1f8b0b732d42461e3174c34104e6beb40467c9b0df42dcd261110ca0f5d3f6e5"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.5.6/kclvm-v0.5.6-linux-amd64.tar.gz"
+      sha256 "ce2ba155013e9bfd7f951e754390a032ed577b98ba8693b8b3e486d9ee091c3b"
     end
   end
 
