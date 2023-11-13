c69200ed1f5a91d2ca8cb2b39792072f3595c09e
peefy <xpf6677@163.com>
2023-11-13 20:30:55 +0800

    chore: bump kcl-lsp and kcl to v0.7.0-alpha.2 versions
    Signed-off-by: peefy <xpf6677@163.com>

--- HomebrewFormula/kcl-lsp@0.7.0.rb
+++ HomebrewFormula/kcl-lsp@0.7.0.rb
@@ -1,24 +1,24 @@
 class KclLspAT070 < Formula
   desc "A constraint-based record & functional language mainly used in configuration and policy scenarios."
   homepage "https://kcl-lang.io"
-  version "0.7.0-alpha.1"
+  version "0.7.0-alpha.2"
   license "Apache License"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.7.0-alpha.1/kclvm-v0.7.0-alpha.1-darwin-arm64.tar.gz"
-      sha256 "10a1a6fec02b15767f2ada7d2e4e4799377771a698a3b151a4cc21b5d5e2c834"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.7.0-alpha.2/kclvm-v0.7.0-alpha.2-darwin-arm64.tar.gz"
+      sha256 "7c157b35910cc72651b5c28684463fbdcba8700a0de0a8d0f345c0b2c317d18b"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.7.0-alpha.1/kclvm-v0.7.0-alpha.1-darwin-amd64.tar.gz"
-      sha256 "46c9342dff4fe62215241a4ee4192e00a90c7f5c644cd32bd16f8327a5046877"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.7.0-alpha.2/kclvm-v0.7.0-alpha.2-darwin-amd64.tar.gz"
+      sha256 "544631966ca8c345e639e964d031e825591abe7da21033b50a00af76e0624d66"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.7.0-alpha.1/kclvm-v0.7.0-alpha.1-linux-amd64.tar.gz"
-      sha256 "6511b50b29da87a09b0d674caefed78ac350b11108248c04f1f35f0ff10a5a53"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.7.0-alpha.2/kclvm-v0.7.0-alpha.2-linux-amd64.tar.gz"
+      sha256 "e44f4dde4fbf86207c513bd69a845b6f389594b62fd303b1deec6f4286a7262e"
     end
   end
 
