63f1e3e2b7f0370529814ef7385e5970f502d446
peefy <xpf6677@163.com>
2024-01-15 19:01:50 +0800

    chore: bump kcl version
    Signed-off-by: peefy <xpf6677@163.com>

--- HomebrewFormula/kcl@0.7.4.rb
+++ HomebrewFormula/kcl@0.7.4.rb
@@ -13,7 +13,7 @@ class KclAT074 < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kcl-lang/cli/releases/download/v0.7.4/kcl-v0.7.4-darwin-arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "d33bdb7ec63c53fc3319011e9626c0505647405c22c2a1851351e975d9d41ea1"
+      sha256 "0d3bfddfe791a3fd14a65ee46ba8e7a43336041f00557e9518ee881306717821"
 
       def install
         bin.install "kcl"
@@ -21,7 +21,7 @@ class KclAT074 < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/kcl-lang/cli/releases/download/v0.7.4/kcl-v0.7.4-darwin-amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "9368b4c72c139404d9a1449eb02ccebac724391d69592caf9b9e8d4cea45191b"
+      sha256 "2c0a6ed1da4eff35c7c020e4ebf8505a8bd032ecc09137db63e6eafe6bd6e612"
 
       def install
         bin.install "kcl"
@@ -30,17 +30,17 @@ class KclAT074 < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kcl-lang/cli/releases/download/v0.7.4/kcl-v0.7.4-linux-arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "65f9ce863465600237d6117a11e16d5ced1567ccdf74563cad7d5a64dadc5988"
+    if Hardware::CPU.intel?
+      url "https://github.com/kcl-lang/cli/releases/download/v0.7.4/kcl-v0.7.4-linux-amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "c2516227c417e0635c9ffd0112e477997ed02e96f6b3fad5e1c227c2e502052b"
 
       def install
         bin.install "kcl"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/cli/releases/download/v0.7.4/kcl-v0.7.4-linux-amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "5c9b12bf3c62fcc092e7438c021d3b0dd5fc5e91b0c304b45f0c807ac5d4509a"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/kcl-lang/cli/releases/download/v0.7.4/kcl-v0.7.4-linux-arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "e5834e59fe72e5b26aeded3053b2e1826b12d5af5d869a19da20f6be43bbab28"
 
       def install
         bin.install "kcl"
