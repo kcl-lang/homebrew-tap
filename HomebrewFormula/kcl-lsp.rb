8959e28eb765293746768bf44fc60a947133df7e
peefy <xpf6677@163.com>
2024-05-01 01:43:14 +0300

    chore: bump kcl-lsp to 0.8.6
    Signed-off-by: peefy <xpf6677@163.com>

--- HomebrewFormula/kcl-lsp.rb
+++ HomebrewFormula/kcl-lsp.rb
@@ -1,28 +1,28 @@
 class KclLsp < Formula
   desc "A constraint-based record & functional language mainly used in configuration and policy scenarios."
   homepage "https://kcl-lang.io"
-  version "0.8.3"
+  version "0.8.6"
   license "Apache License"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.8.3/kclvm-v0.8.3-darwin-amd64.tar.gz"
-      sha256 "f1825109c6ab9f54c7559fc1dc706a773aa5f675bc16ef0b0fc68fa8be0378d0"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.8.6/kclvm-v0.8.6-darwin-amd64.tar.gz"
+      sha256 "5ef91be3b38f130eaae9ad00478471493df89c012eb70f8517a22217c031a230"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.8.3/kclvm-v0.8.3-darwin-arm64.tar.gz"
-      sha256 "6204aa3659f87b618e08c60bab6c05c238aab20daa4f2fcc6b4c9b7160725a6e"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.8.6/kclvm-v0.8.6-darwin-arm64.tar.gz"
+      sha256 "cef60e048d257bd3950a94f7484c009418f044d51c1c43a46e61cf217f89cf58"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.8.3/kclvm-v0.8.3-linux-amd64.tar.gz"
-      sha256 "52f49026a7767388ffa6147138d770314e1554ec437619abcde6f3bd0a1a1563"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.8.6/kclvm-v0.8.6-linux-amd64.tar.gz"
+      sha256 "fbcc4b1ce92db66969a7559c7d3beea1a3f5dfadffe407d1f655ad55ea2b1b4c"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.8.3/kclvm-v0.8.3-linux-arm64.tar.gz"
-      sha256 "49698899fdce62f6806d8c11402cd10ea86e35762baae7283e5f5338dbb0bd58"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.8.6/kclvm-v0.8.6-linux-arm64.tar.gz"
+      sha256 "e5d23434198f4222ee98d1601c59e8754e3fc48ee9ad9e924e2d0fbe7133ee30"
     end
   end
 
