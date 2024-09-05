69daafde2188d6d82545f7d528359faf23f9f255
peefy <xpf6677@163.com>
2024-09-05 19:37:38 +0800

    chore: bump kcl and kcl-lsp to v0.10.0-rc.1
    Signed-off-by: peefy <xpf6677@163.com>

--- HomebrewFormula/kcl-lsp.rb
+++ HomebrewFormula/kcl-lsp.rb
@@ -1,28 +1,28 @@
 class KclLsp < Formula
   desc "A constraint-based record & functional language mainly used in configuration and policy scenarios."
   homepage "https://kcl-lang.io"
-  version "0.10.0-beta.1"
+  version "0.10.0-rc.1"
   license "Apache License"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0-beta.1/kclvm-v0.10.0-beta.1-darwin-amd64.tar.gz"
-      sha256 "963cce7cd298989a37515153e802f642e177e3c6ba3c8a3e1e852da2451fd2c6"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0-rc.1/kclvm-v0.10.0-rc.1-darwin-amd64.tar.gz"
+      sha256 "d6485342381456c86e0b37ebcadf94e0237d24de08cbebddb0a29e445f30d95f"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0-beta.1/kclvm-v0.10.0-beta.1-darwin-arm64.tar.gz"
-      sha256 "ec0eee82cf6abc579eb3bd614dfbd471f42d8f5e25f49c29b34d28e1b3e2fa51"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0-rc.1/kclvm-v0.10.0-rc.1-darwin-arm64.tar.gz"
+      sha256 "4215200f4b21b061d45d02fce8b3485cb7ad9d8e89e531ffe459abb217963cf5"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0-beta.1/kclvm-v0.10.0-beta.1-linux-amd64.tar.gz"
-      sha256 "7888884d9083772acfb7435806a7e75bdbee1800bbd802a200fdf7a3cdcef9fa"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0-rc.1/kclvm-v0.10.0-rc.1-linux-amd64.tar.gz"
+      sha256 "9ad67df74b6bc9ab1cedf09631c8e8cadd143b55d8841f5c415f8b004bd91ed4"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0-beta.1/kclvm-v0.10.0-beta.1-linux-arm64.tar.gz"
-      sha256 "e8fd1a7ab2e196497a060733a667b8051079ae78321b31838668fc0625b7c487"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0-rc.1/kclvm-v0.10.0-rc.1-linux-arm64.tar.gz"
+      sha256 "ee8dccb0cdd16797241854de3d634474215a1239798276fd500ac22a253bb907"
     end
   end
 
--- HomebrewFormula/kcl-lsp@0.10.0.rb
+++ HomebrewFormula/kcl-lsp@0.10.0.rb
@@ -1,4 +1,4 @@
-class KclLspAT090 < Formula
+class KclLspAT0100 < Formula
   desc "A constraint-based record & functional language mainly used in configuration and policy scenarios."
   homepage "https://kcl-lang.io"
   version "0.10.0-beta.1"
