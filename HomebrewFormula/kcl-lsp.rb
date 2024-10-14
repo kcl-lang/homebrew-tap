b2867a9bab1d53fe288cf510678e3ef4658c3423
peefy <xpf6677@163.com>
2024-10-14 22:14:41 +0800

    chore: bump kcl-lsp to v0.10.3
    Signed-off-by: peefy <xpf6677@163.com>

--- HomebrewFormula/kcl-lsp.rb
+++ HomebrewFormula/kcl-lsp.rb
@@ -1,28 +1,28 @@
 class KclLsp < Formula
   desc "A constraint-based record & functional language mainly used in configuration and policy scenarios."
   homepage "https://kcl-lang.io"
-  version "0.10.0"
+  version "0.10.3"
   license "Apache License"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0/kclvm-v0.10.0-darwin-amd64.tar.gz"
-      sha256 "d574568e8ec0b771672ff19b5f7b4d738cc239789207985c51e8304432e5c6b6"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.3/kclvm-v0.10.3-darwin-amd64.tar.gz"
+      sha256 "0c8db6d6e712f8e95c3a230ae50e7634586f36b850a03f165c024b571075eb0f"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0/kclvm-v0.10.0-darwin-arm64.tar.gz"
-      sha256 "beced3b58c4abaca86ac5a5079d763e16a5485f1c3e696f32f94673b32455def"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.3/kclvm-v0.10.3-darwin-arm64.tar.gz"
+      sha256 "8c08c0b675c65cdaa6c4609da179c5ea60545c04d1f5e0ad20945ca098f7efbc"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0/kclvm-v0.10.0-linux-amd64.tar.gz"
-      sha256 "7d1339763ea39facb3319ddc313f3449aa71dcdd935ef7ee555e21e11b4ad69f"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.3/kclvm-v0.10.3-linux-amd64.tar.gz"
+      sha256 "3d0bd10c0121df6d639991cce80bf159e66e0b4e29a0a92f6b23cda9391ca0e1"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.0/kclvm-v0.10.0-linux-arm64.tar.gz"
-      sha256 "97ddfeea1e7f64de2623d83116ab7d6572f4960a1633843c34811bdc5fe4a5f1"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.10.3/kclvm-v0.10.3-linux-arm64.tar.gz"
+      sha256 "504217ce633fd77839e187dade6834a452f46f2858fbf7a9507f82399cb4fd8a"
     end
   end
 
