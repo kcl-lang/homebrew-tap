5893d1510bd90ad7afb1f1c31bdad009523b1be1
Peefy <xpf6677@163.com>
2026-09-26 02:01:53 +0800

    feat(kcl-lsp): bump to v0.13.0 (per-binary release)

--- HomebrewFormula/kcl-lsp.rb
+++ HomebrewFormula/kcl-lsp.rb
@@ -1,34 +1,32 @@
 class KclLsp < Formula
   desc "A constraint-based record & functional language mainly used in configuration and policy scenarios."
   homepage "https://kcl-lang.io"
-  version "0.11.2"
+  version "0.13.0"
   license "Apache License"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.11.2/kclvm-v0.11.2-darwin-amd64.tar.gz"
-      sha256 "76f231095b69965d5d52e3d054d916fb9be535e1d7172f97fd3b3f68ab582c52"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.13.0/kcl-language-server-v0.13.0-darwin-amd64.tar.gz"
+      sha256 "cd4d1828a4cdc3224a99f42227bdc0e19a0dd6d97a93c297f752050b7d6f2f9b"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.11.2/kclvm-v0.11.2-darwin-arm64.tar.gz"
-      sha256 "89c315a7b280a12cd44d0e81a5135805229f56f8bff04c90c76dbb8b1ddb1427"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.13.0/kcl-language-server-v0.13.0-darwin-arm64.tar.gz"
+      sha256 "48ec9e765b134cf4add04ef8963442468c38bbe3f2ad8c1f0553130d7ebff7bc"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.11.2/kclvm-v0.11.2-linux-amd64.tar.gz"
-      sha256 "e1ecd170373c43a6755afd47a1f4146be03a4a61499065e0b7d1d62cc5e69405"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.13.0/kcl-language-server-v0.13.0-linux-amd64.tar.gz"
+      sha256 "fb8205232206845ae5b833410c3166248fd38f3b289df3dc6bbaca1f42cedb14"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kcl-lang/kcl/releases/download/v0.11.2/kclvm-v0.11.2-linux-arm64.tar.gz"
-      sha256 "035b3b37242811a7a3c2dacee810bf9f7eef0ddd5296876aa71f346b0adc3805"
+      url "https://github.com/kcl-lang/kcl/releases/download/v0.13.0/kcl-language-server-v0.13.0-linux-arm64.tar.gz"
+      sha256 "3f65d9d20fb5d9c0f06f51f86e4f6a62e22f05052a261ed1cf5db2ddda2c0e6e"
     end
   end
 
   def install
-    prefix.install_metafiles
-    libexec.install Dir["*"]
-    bin.install "#{libexec}/bin/kcl-language-server"
+    bin.install "kcl-language-server"
   end
 end
